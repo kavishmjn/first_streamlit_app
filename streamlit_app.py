90a2d3f526dd8c644982c32ba4da5e00b9dedc60
Kavish Mahajan <49091566+kavishmjn@users.noreply.github.com>
2023-04-10 11:59:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,4 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-fruits = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-fruits-fruits.set_index('Fruit')
-#selecting fruit
-streamlit.multiselect("Pick some fruits: ", list(fruits.index))
-streamlit.dataframe(fruits)
+
