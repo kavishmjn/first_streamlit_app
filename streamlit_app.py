b830c05bf070a3e1f4a35dfaec52bad5f1c819da
Kavish Mahajan <49091566+kavishmjn@users.noreply.github.com>
2023-04-10 12:02:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
-
+fruits = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = fruits.set_index('Fruit')
+streamlit.dataframe(my_fruit_list)
